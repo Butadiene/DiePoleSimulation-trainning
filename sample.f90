c0b394300288299795307c312ef13b0123bd3d37
butadiene <najimi0510@gmail.com>
2020-04-26 16:03:56 +0900

    #4 書き出しクラスの実装

--- sample.f90
+++ sample.f90
@@ -2,19 +2,22 @@ program main
     use NumericalAnalytics_class
     use Malthus_class
     use Euler_class
+    use Writeout_class
     implicit none
  
     class(ODE),allocatable::sys
     class(Scheme),allocatable::instanciateScheme
     class(NumericalAnalytics),allocatable::instanciateNumericalAnalytics
+    class(Writeout),allocatable::instanciatewriteout
     DOUBLE PRECISION :: a(3) =  (/ 1.0d0, 2.0d0, 3.0d0 /)
     DOUBLE PRECISION :: init(3) = (/ 1.0d0,1.0d0,1.0d0 /)
-
+    CHARACTER(32) :: filename = 'test.csv' 
 
     sys = Malthus(a,init)
     instanciateScheme = Euler(0.01d0,300)
     instanciateNumericalAnalytics = NumericalAnalytics(sys, instanciateScheme)
-    write(6,*) instanciateNumericalAnalytics%getSolution()
-
+    instanciatewriteout = Writeout(instanciateNumericalAnalytics%getSolution())
+    call instanciatewriteout%WritetoCSV(filename)
+    
  
  end program main
--- writeout_class.f90
+++ writeout_class.f90
@@ -1,5 +1,39 @@
 module Writeout_class
     implicit none
-    
+    type Writeout
+        PRIVATE
+        DOUBLE PRECISION,allocatable::solution(:,:)
+    contains 
+        procedure :: WritetoCSV => Writeout_WritetoCSV
+    end type Writeout
+
+    interface Writeout
+        module procedure init_Writeout
+    end interface Writeout
+
+    contains
+    type(Writeout) function init_Writeout(inputarray)
+        DOUBLE PRECISION::inputarray(:,:)
+        init_Writeout%solution = inputarray
+    end function init_Writeout
+
+    subroutine Writeout_WritetoCSV(this,filename)
+        class(Writeout),intent(in)::this
+        CHARACTER(32) :: filename
+        INTEGER dimsize(2)
+        INTEGER i,j
+        dimsize = shape(this%solution)
+        open(17, file=filename, status='replace')
+        do i=1,dimsize(2)
+            do  j =1,dimsize(1)
+                write (17,"(f10.5)",advance='no') this%solution(j,i)
+                if(j<dimsize(1)) then
+                    write (17,"(A)",advance='no') ','
+                end if
+            end do
+            write (17,"(A)") ''
+        end do
+        close(17)
+    end subroutine Writeout_WritetoCSV
 
 end module Writeout_class
